65cf501b175c08de54a776349dc946f08e1dcd4a
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-05-06 23:50:28 +0200

    initial

---
+++ mon projet web/JAVA SCRIPT/script.js
@@ -0,0 +1,13 @@
+// var n1= parseInt(prompt("Entrer le premier nombre : "));
+// var n2= parseInt(prompt("Entrer le deuxieme nombre  : "));
+// if((n1+n2)%2==0){
+//     alert("la somme des nombres "+n1+" et "+n2+" est paire");
+// }
+// else{
+//     alert("la somme des nombres "+n1+" et "+n2+" est impaire");
+// }
+
+var n1=parseInt(prompt("Entrer le nombre de fois : "));
+for(var a=0;a<n1;a++){
+    alert("Bonjour Boss");
+}
---
+++ mon projet web/JAVA SCRIPT/script2.js
@@ -0,0 +1,9 @@
+var choix=prompt("Entrer votre choix, Fr pour Francais et En pour Anglais");
+switch(choix){
+    case 'Fr':
+    alert("Bonjour");
+    case 'En':
+    alert("Morning");    
+    default:
+    alert("Valeur invalide. entrer un autre");
+}
